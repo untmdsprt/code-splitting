44900af442b63d0dda47a7d77bef594fda228182
Jenny <untmdsprt@gmail.com>
2018-10-02 19:49:19 -0500

    S5L95 - Code Splitting Introduction

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Page1 />
+        <Page2 />
+        <Page3 />
       </div>
     );
   }
