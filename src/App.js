680b7e1fe06c138a275a640c7ff2d27136722507
Jenny <untmdsprt@gmail.com>
2018-10-03 21:43:19 -0500

    S5L97 - Code splitting Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
 class App extends Component {
   constructor() {
@@ -9,17 +12,19 @@ class App extends Component {
       route: 'page1'
     }
   }
+
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <Page1 />
-        <Page2 />
-        <Page3 />
-      </div>
-    );
+    if (this.state.route === 'page1'){
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page2') {
+        return <Page2 onRouteChange={this.onRouteChange} />
+    } else if (this.state.route === 'page3') {
+      return <Page3 onRouteChange={this.onRouteChange} />
+    }
   }
 }
 
